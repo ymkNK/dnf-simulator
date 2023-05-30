0429ca5cf16ebe8dcccead42f0747ad8cf382e5b
yangmingkai <yangmingkai@bytedance.com>
2023-05-30 20:17:47 +0800

    add max continuous fail cnt
    Change-Id: I85044e96d2e1bf96875df6526e6e920377588907

--- dnf.go
+++ dnf.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
 func main() {
-	itemCnt := 0       // 胚子数量
-	upgradeCnt := 0    // 增幅次数
-	succCnt := 0       // 成功次数
-	failCnt := 0       // 失败次数
-	currentLevel := 12 // 胚子等级
-	targetLevel := 18  // 目标等级
+	itemCnt := 0                  // 胚子数量
+	upgradeCnt := 0               // 增幅次数
+	succCnt := 0                  // 成功次数
+	failCnt := 0                  // 失败次数
+	maxContinuousFailCnt := 0     // 最大连续失败数
+	startLevel := 12              // 胚子等级
+	targetLevel := 18             // 目标等级
+	currentLevel := startLevel    // 起始等级
+	currentContinuousFailCnt := 0 // 当前连续失败数
 
 	for {
 		if currentLevel == targetLevel {
 			break
 		}
 
-		if currentLevel == 12 {
+		if currentLevel == startLevel {
 			itemCnt++
 		}
 
@@ -28,15 +32,30 @@ func main() {
 		if UpgradeOnce() {
 			currentLevel++
 			succCnt++
+			maxContinuousFailCnt = int(math.Max(float64(currentContinuousFailCnt), float64(maxContinuousFailCnt)))
+
+			currentContinuousFailCnt = 0
+
 			continue
 		}
 
-		currentLevel = 12
+		currentLevel = startLevel
 		failCnt++
+		currentContinuousFailCnt++
 	}
 
-	fmt.Printf("item cnt: %d\nupgrade cnt: %d\nsucc cnt: %d\nfail cnt: %d\nsucc rate: %f",
-		itemCnt, upgradeCnt, succCnt, failCnt, float32(succCnt)/float32(upgradeCnt)*100)
+	fmt.Printf("胚子数量: %d\n"+
+		"增幅次数: %d\n"+
+		"成功次数: %d\n"+
+		"失败次数: %d\n"+
+		"最大连败次数: %d\n"+
+		"成功率: %f",
+		itemCnt,
+		upgradeCnt,
+		succCnt,
+		failCnt,
+		maxContinuousFailCnt,
+		float32(succCnt)/float32(upgradeCnt)*100)
 }
 
 var r = rand.New(rand.NewSource(time.Now().Unix()))
